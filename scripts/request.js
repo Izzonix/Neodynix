99b1b0488a41e2e507b969034afc755c6d1ca43d
Izzonix <isaacsemwogerere37@gmail.com>
2025-07-07 02:47:04 +0300

    Update request.js

--- scripts/request.js
+++ scripts/request.js
@@ -1,13 +1,12 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const emailInput = document.getElementById("email");
-  const followupInput = document.getElementById("followup-link");
-
-  if (emailInput && followupInput) {
-    emailInput.addEventListener("input", function () {
-      const email = emailInput.value.trim();
-      followupInput.value = `https://izzonix.github.io/neodynix/followup.html?email=${encodeURIComponent(email)}`;
-    });
-  } else {
-    console.error("Email input or hidden followup field missing.");
-  }
-});
+await Backendless.Messaging.sendEmailFromTemplate(
+  "WebsiteRequest", // your template name
+  {
+    name,
+    email,
+    category,
+    template,
+    details,
+    followup_link
+  },
+  email
+);
