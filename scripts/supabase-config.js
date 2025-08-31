4a68f202b8496d330bee76d041c1a1dabb412c9a
Izzonix <isaacsemwogerere37@gmail.com>
2025-08-31 23:19:56 +0300

    Update supabase-config.js

--- scripts/supabase-config.js
+++ scripts/supabase-config.js
@@ -2,6 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://spnxywyrjbwbwntblcjl.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNwbnh5d3lyamJ3YndudGJsY2psIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY2NDc2NDYsImV4cCI6MjA3MjIyMzY0Nn0.Kjta7PEMwTcq1uu5RuKYKvEPPjpR3ko5KCwR8Qh4b6E';
-const supabase = createClient( https://spnxywyrjbwbwntblcjl.supabase.co, eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNwbnh5d3lyamJ3YndudGJsY2psIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY2NDc2NDYsImV4cCI6MjA3MjIyMzY0Nn0.Kjta7PEMwTcq1uu5RuKYKvEPPjpR3ko5KCwR8Qh4b6E);
 
-export { supabase };
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export { supabase, supabaseUrl };
